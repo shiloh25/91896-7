f129977465327286d9f7f3acd69eb5d713bac3f7
shiloh25 <165841775+shiloh25@users.noreply.github.com>
2024-04-05 13:17:04 +1300

    Add files via upload

---
+++ 07_string_checker.py
@@ -0,0 +1,8 @@
+# checks that users enter a valid response (eg yes / no or cash / credit) based on a list of options
+def string_checker(question, num_letters, valid_responses):
+    pass
+
+
+# main routine goes here
+yes_no_list = ["yes", "no"]
+payment_list = ["cash", "credit"]
---
+++ ZZ_sandpit.py
@@ -0,0 +1,6 @@
+# a pla area for experiementing
+
+word = "credit"
+print(word[0])
+
+print(word[:2])
