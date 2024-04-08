6fd02296db074af107cd85fe48355502f3ddddd6
shiloh25 <165841775+shiloh25@users.noreply.github.com>
2024-04-08 14:31:52 +1200

    Add files via upload

--- 00_MM_base_v01.py
+++ 00_MM_base_v01.py
@@ -1,21 +1,4 @@
 # functions go here
-
-# Checks user has entered yes / no to a question
-def yes_no(question):
-
-    while True:
-        response = input(question).lower()
-
-        if response == "yes" or response == "y":
-            return "yes"
-
-        elif response == "no" or response == "n":
-            return "no"
-
-        else:
-            print("please enter yes or no")
-
-
 # checks that user response is not blank
 def not_blank(question):
 
@@ -60,13 +43,36 @@ def calc_ticket_price(var_age):
     return price
 
 
+# checks that users enter a valid response (eg yes / no or cash / credit) based on a list of options
+def string_checker(question, num_letters, valid_responses):
+
+    error = "Please choose {} or {}".format(valid_responses[0], valid_responses[1])
+
+    if num_letters == 1:
+        short_version = 1
+    else:
+        short_version = 2
+
+    while True:
+        response = input(question).lower()
+
+        for item in valid_responses:
+            if response == item[:short_version] or response == item:
+                return item
+
+        print(error)
+
+
 # main routine starts here
 
 # set maximum number of tickets below
 MAX_TICKETS = 3
 tickets_sold = 0
+
+yes_no_list = ["yes", "no"]
+payment_list = ["cash", "credit"]
 # Ask the user if they want instructions
-want_instructions = yes_no("Do you want to read the instructions? ")
+want_instructions = string_checker("Do you want to read the instructions? ", 1, yes_no_list)
 
 if want_instructions == "yes" or want_instructions == "y":
     print("instructions go here")
@@ -95,6 +101,8 @@ while tickets_sold < MAX_TICKETS:
     ticket_cost = calc_ticket_price(age)
     print("Age: {}, Ticket Price: ${:.2f}".format(age, ticket_cost))
 
+    # get payment method
+    pay_method = string_checker("Choose a payment method (cash / credit): ", 2, payment_list)
     tickets_sold += 1
 
 # Output number of tickets sold
